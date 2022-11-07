274abbb960bce64ec8359034cda07692b6890952
haonhat0703 <haonhat2729@gmail.com>
2022-11-07 23:17:01 +0700

    add some feature square

--- src/js/game.js
+++ src/js/game.js
@@ -1,4 +1,5 @@
 square = document.querySelector("#game .left");
+
 //repair screen
 const _repair = () => {
   for (let i = 0; i < 81; i++)
@@ -106,9 +107,34 @@ sq_game.forEach((sq, index) => {
   });
 });
 
+const _checkMask = () => {
+  for (let i = 0; i < table.length; i++)
+    if (table[i] != tableR[i]) return false;
+  return true;
+};
+const _restart = () => {
+  current_square = -1;
+  for (let i = 0; i < table.length; i++)
+    if (tableG[i] == 0) {
+      table[i] = 0;
+      sq_game[i].innerText = "";
+    }
+  _removeToggle();
+  _check_err();
+  _checkSame();
+};
+
 const _checkFull = () => {
   for (val of table) if (val == 0) return;
-  alert("OKKKKKKK");
+  if (_checkMask()) {
+    setTimeout(() => {
+      if (confirm("Good Job Bro <3. Press ok to restart game")) _restart();
+    }, 100);
+  } else {
+    setTimeout(() => {
+      alert("Wrong Answer Bro </>");
+    }, 100);
+  }
 };
 //[]{}
 const _no_tick = (index) => {
@@ -121,12 +147,48 @@ const _no_tick = (index) => {
   _checkSame(index);
   _check_err();
 };
+//[]{}
+const _magic_square = (row, col) => {
+  let index = 0;
+  if (_checkMask()) return;
+  for (let i = row; i < row + 3; i++) {
+    for (let j = col; j < col + 3; j++) {
+      index++;
+      setTimeout(() => {
+        let idx = i * 9 + j;
+        let tmp = sq_game[idx].style.backgroundColor;
+        sq_game[idx].style.setProperty(
+          "background-color",
+          "#d1dbef",
+          "important"
+        );
+        setTimeout(() => {
+          sq_game[idx].style.backgroundColor = tmp;
+        }, index * 40);
+      }, index * 80);
+    }
+  }
+};
+
+const _check_ok_square = (index) => {
+  let df = _info_square(index);
+  let rowF = Math.floor(df.square / 3) * 3;
+  let colF = (df.square % 3) * 3;
+  for (let i = rowF; i < rowF + 3; i++)
+    for (let j = colF; j < colF + 3; j++) {
+      if (table[i * 9 + j] == 0 || table[i * 9 + j] != tableR[i * 9 + j])
+        return;
+    }
+  _magic_square(rowF, colF);
+};
+
 const _tick = (index, val) => {
   sq_game[index].classList.remove("err");
   sq_game[index].classList.remove("true");
   sq_game[index].classList.remove("false");
   sq_game[index].innerText = val;
   table[index] = +val;
+  _check_ok_square(index);
   if (table[index] == tableR[index]) sq_game[index].classList.add("true");
   else sq_game[index].classList.add("false");
   _checkSame(index);
@@ -174,13 +236,18 @@ document.addEventListener("keydown", (e) => {
 });
 
 const _ok_key = (key) => {
+  if (key == "Delete" || key == "Backspace") return true;
   for (let i = 1; i < 10; i++) if (+key == i) return true;
   return false;
 };
 document.addEventListener("keydown", (e) => {
   if (current_square != -1 && _ok_key(e.key)) {
     if (tableG[current_square] == 0) {
-      if (sq_game[current_square].innerText == e.key) {
+      if (
+        sq_game[current_square].innerText == e.key ||
+        e.key == "Delete" ||
+        e.key == "BackSpace"
+      ) {
         _no_tick(current_square);
       } else {
         _tick(current_square, e.key);
@@ -189,7 +256,6 @@ document.addEventListener("keydown", (e) => {
     }
   }
 });
-
 //default value//[]{}
 sq_game.forEach((sq, index) => {
   if (tableG[index] != 0) {
@@ -201,9 +267,11 @@ sq_game.forEach((sq, index) => {
 // model
 open = document.querySelector(".open");
 nRight = document.querySelector(".right-container");
+open.innerHTML = `<i class="fa-solid fa-bars"></i>`;
 
 open.addEventListener("click", () => {
   if (open.innerHTML == `<i class="fa-solid fa-bars"></i>`) {
+    console.log("aaa");
     open.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
     nRight.style.display = "flex";
   } else {
